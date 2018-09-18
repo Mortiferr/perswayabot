926f8d69186961d57e555d948ae8fff38a2efbe9
Patrick Black Jr <info@patrickblackjr.com>
2018-09-18 11:59:58 -0500

    In-progress auto regen role

--- index.js
+++ index.js
@@ -53,6 +53,63 @@ client.on("message", message => {
         .send(`${message.author} - ${arg1}`);
       member.addRole(role).catch(console.error);
     }
+  } else
+  if (command === 'resetqueue') {
+
+    // Remove all from Queued role
+
+    // Purge messages
+    const user = message.mentions.users.first();
+    const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
+    if (!amount) return message.reply('Must specify an amount to delete! *Note: Discord limits you to a maximum of 100 at a time.*');
+    if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
+    message.channel.fetchMessages({
+      limit: 100,
+    }).then((messages) => {
+      if (user) {
+        const filterBy = user ? user.id : Client.user.id;
+        messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+      }
+      message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+    });
+  } else
+  if (command === "beginqueue") {
+    message.guild.createRole({
+      data: {
+        name: "Queued",
+        hoist: true,
+        mentionable: true,
+        permissions: {
+          CREATE_INSTANT_INVITE: true,
+          KICK_MEMBERS: true,
+          BAN_MEMBERS: true,
+          ADMINISTRATOR: true,
+          MANAGE_CHANNELS: true,
+          MANAGE_GUILD: true,
+          ADD_REACTIONS: true,
+          READ_MESSAGES: true,
+          SEND_MESSAGES: true,
+          SEND_TTS_MESSAGES: true,
+          MANAGE_MESSAGES: true,
+          EMBED_LINKS: true,
+          ATTACH_FILES: true,
+          READ_MESSAGE_HISTORY: true,
+          MENTION_EVERYONE: true,
+          EXTERNAL_EMOJIS: true,
+          CONNECT: true,
+          SPEAK: true,
+          MUTE_MEMBERS: true,
+          DEAFEN_MEMBERS: true,
+          MOVE_MEMBERS: true,
+          USE_VAD: true,
+          CHANGE_NICKNAME: true,
+          MANAGE_NICKNAMES: true,
+          MANAGE_ROLES_OR_PERMISSIONS: true,
+          MANAGE_WEBHOOKS: true,
+          MANAGE_EMOJIS: true
+        }
+      }
+    })
   }
 });
 
