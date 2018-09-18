1a53d51c4c06b397064b38d73308ebbb4061e015
Patrick Black Jr <info@patrickblackjr.com>
2018-09-18 01:57:19 -0500

    Move variables to config

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const Discord = require('discord.js');
 // new Discord.Client instance
 const client = new Discord.Client();
 
-// variables
-const secretChannelForHunterId = '491466153886220288';
-const secretChannelForSurvivorId = "491494829558202368";
-
 client.on('ready', async () => {
   console.log(`${client.user.username} loaded successfully.`)
 });
@@ -25,15 +21,15 @@ client.on("message", message => {
     let hunterId = args[0];
     message.reply(`I've added you to the queue for Hunter. Remeber this is **NOT** a guarantee, it is a *queue*. Your ID you entered is: ${hunterId}`);
     client.channels
-      .get(secretChannelForHunterId)
-      .send(`${message.author} - ${hunterId}`)
+      .get(config.channelIds.hunters)
+      .send(`${message.author} - ${hunterId}`);
   } else
     if (command === 'customsurvivor') {
       let survivorId = args[0];
       message.reply(`I've added you to the queue for Survivor. Remeber this is **NOT** a guarantee, it is a *queue*. Your ID you entered is: ${survivorId}`);
       client.channels
-        .get(secretChannelForSurvivorId)
-        .send(`${message.author} - ${survivorId}`)
+        .get(config.channelIds.survivors)
+        .send(`${message.author} - ${survivorId}`);
     }
 });
 
