26ab5bfc127e21fcefc1987fc06acd5191ddf061
Patrick Black Jr <info@patrickblackjr.com>
2018-09-20 11:47:14 -0500

    Remove reset queue command for now

--- index.js
+++ index.js
@@ -59,35 +59,6 @@ client.on('message', message => {
       member.addRole(role).catch(console.error);
     }
   }
-  else if (command === 'resetqueue') {
-    // Remove all from Queued role
-
-    // Purge messages
-    const user = message.mentions.users.first();
-    const amount = !!parseInt(message.content.split(' ')[1])
-      ? parseInt(message.content.split(' ')[1])
-      : parseInt(message.content.split(' ')[2]);
-    if (!amount)
-      return message.reply(
-        `Must specify an amount to delete! *Note: Discord limits you to a maximum of 100 at a time.*`
-      );
-    if (!amount && !user)
-      return message.reply(
-        `Must specify a user and amount, or just an amount, of messages to purge!`
-      );
-    message.channel.fetchMessages({ limit: 100 }).then(messages => {
-      if (user) {
-        const filterBy = user ? user.id : Client.user.id;
-        messages = messages
-          .filter(m => m.author.id === filterBy)
-          .array()
-          .slice(0, amount);
-      }
-      message.channel
-        .bulkDelete(messages)
-        .catch(error => console.log(error.stack));
-    });
-  }
 });
 
 client.login(config.dev.token); // TODO: change in production
