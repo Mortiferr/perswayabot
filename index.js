27fb2f1dcd32129be97f831d6bbf20936789ca91
Patrick Black Jr <info@patrickblackjr.com>
2018-09-21 23:45:43 -0500

    Seperate !customhunter/survivor to !custom[space]hunter/survivor

--- index.js
+++ index.js
@@ -19,45 +19,48 @@ client.on('message', message => {
     .split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  if (command === 'customhunter') {
-    let arg1 = args[0];
-    let arg2 = args[1];
+  if (command === 'custom') {
+    let team = args[0];
+    let id   = args[1];
+    let ign  = args[2];
     let role = message.guild.roles.get(config.dev.roleId); // TODO: change in production
     let member = message.member;
-    if (message.member.roles.has(role.id)) {
-      message.reply(
-        `You're already in the queue. You can't join again. Sorry!`
-      );
-    } 
-    else {
-      message.reply(
-        `I've added you to the queue for Hunter. Remember this is **NOT** a guarantee. Your ID you entered is: ${arg1} and the IGN you entered is: ${arg2}.`
-      );
-      client.channels
-        .get(config.dev.channelIds.hunters) // TODO: change in production
-        .send(`${message.author} - ${arg1} - ${arg2}`);
-      member.addRole(role).catch(console.error);
+
+    if (team === 'hunter') {
+      if (message.member.roles.has(role.id)) {
+        message.reply(
+          `You're already in the queue. You can't join again. Sorry!`
+        );
+      }
+      else {
+        message.reply(
+          `I've added you to the queue for **${team}**. Remember this is *NOT* a guarantee. Your ID you entered is: **${id}** and the IGN you entered is: **${ign}**.`
+        );
+        client.channels
+          .get(config.dev.channelIds.hunters) // TODO: change in production
+          .send(`${message.author} - ${id} - ${ign}`);
+        member.addRole(role).catch(console.error);
+      }
+    }
+
+    else if (team === 'survivor') {
+      let role = message.guild.roles.get(config.dev.roleId); // TODO: change in production
+      let member = message.member;
+      if (message.member.roles.has(role.id)) {
+        message.reply(
+          `You're already in the queue. You can't join again. Sorry!`
+        );
+      } else {
+        message.reply(
+          `I've added you to the queue for **${team}**. Remember this is *NOT* a guarantee. Your ID you entered is: **${id}** and the IGN you entered is: **${ign}**.`
+        );
+        client.channels
+          .get(config.dev.channelIds.survivors) // TODO: change in production
+          .send(`${message.author} - ${id} - ${ign}`);
+        member.addRole(role).catch(console.error);
+      }
     }
   } 
-  else if (command === 'customsurvivor') {
-    let arg1 = args[0];
-    let arg2 = args[1];
-    let role = message.guild.roles.get(config.dev.roleId); // TODO: change in production
-    let member = message.member;
-    if (message.member.roles.has(role.id)) {
-      message.reply(
-        `You're already in the queue. You can't join again. Sorry!`
-      );
-    } else {
-      message.reply(
-        `I've added you to the queue for Survivor. Remember this is **NOT** a guarantee. Your ID you entered is: ${arg1} and the IGN you entered is: ${arg2}.`
-      );
-      client.channels
-        .get(config.dev.channelIds.survivors) // TODO: change in production
-        .send(`${message.author} - ${arg1} - ${arg2}`);
-      member.addRole(role).catch(console.error);
-    }
-  }
 });
 
 client.login(config.dev.token); // TODO: change in production
