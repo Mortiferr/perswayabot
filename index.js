08f22f5ed0e5afac689267b4da370032ece27214
Patrick Black Jr <info@patrickblackjr.com>
2018-09-23 20:27:17 -0500

    major security fixes and deployment simplifications

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const config = require('./config.json');
+const dotenv = require('dotenv').config();
+// const config = require('./config.json');
 const Discord = require('discord.js');
 const mongoose = require('mongoose');
-const playerBase = require('./db/player.model.js')
+// const playerBase = require('./db/player.model.js')
 
-let dev_db_url = 'mongodb://patrickblackjr:remmie100600@ds123971.mlab.com:23971/playerbase'
+let dev_db_url = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ds123971.mlab.com:23971/playerbase`
 let mongodb = mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 mongoose.connect(mongoDB);
@@ -19,22 +20,38 @@ const client = new Discord.Client();
 
 client.on('ready', async () => {
   console.log(`${client.user.username} loaded successfully.`);
+  client.user.setPresence({ game: { name: 'with you' }, status: 'online' })
+    .catch(console.error);
 });
 
 client.on('message', message => {
   if (message.author.bot) return;
   
   const shortLinks = ["goo.gl", "shorte.st", "adf.ly", "bc.vc", "bit.ly", "bit.do", "soo.gd", "7.ly", "5.gp", "tiny.cc", "ouo.io", "zzb.bz", "adfoc.us", "my.su"]
-  const swearWords = ["faggot", "gini", "kike", "n1gga", "n1gger", "nigg3r", "nigga", "nigger", "retard", "niqqa", "n1qqa", "niqqer", "n1qqer", "apples"]
-  
+  const swearWords = ["faggot", "gini", "kike", "n1gga", "n1gger", "nigg3r", "nigga", "nigger", "retard", "niqqa", "n1qqa", "niqqer", "n1qqer",]
+  const reaction = ["apple", "lol", "persway"]
+
+  if (message.content === 'apples') {
+    const apples = client.emojis.find(emoji => emoji.name === 'apple')
+    message.reply(`${apples}`)
+  }
+
   if (swearWords.some(word => message.content.toLowerCase().includes(word))) {
     message.delete();
     message.author.send(`Hey, please don't use that word in Perswayable's Discord. Thanks.`);
+    client.channels
+      .find(c => c.name === 'logs')
+      .send(`${message.author} posted a racist / sexist / disablist slur that was deleted by ${client.user.username}. They've been warned via DM.`)
+      .catch(console.error)
   }
 
   if (shortLinks.some(word => message.content.includes(word))) {
     message.delete();
     message.author.send(`Hey, please don't send short links in Perswayable's Discord. You can post the link, just send the full length one. Thanks :)`);
+    client.channels
+      .find(c => c.name === 'logs')
+      .send(`${message.author} posted a link that was deleted by ${client.user.username}. They've been warned via DM.`)
+      .catch(console.error)
   }
   
   if (message.content.indexOf(config.prefix) !== 0) return;
@@ -49,7 +66,7 @@ client.on('message', message => {
     let team = args[0];
     let id   = args[1];
     let ign  = args[2];
-    let role = message.guild.roles.get(config.dev.roleId); // TODO: change in production
+    let role = message.guild.roles.get(process.env.QED); // TODO: change in production
     let member = message.member;
 
     if (team === 'hunter') {
@@ -63,14 +80,14 @@ client.on('message', message => {
           `I've added you to the queue for **${team}**. Remember this is *NOT* a guarantee. Your ID you entered is: **${id}** and the IGN you entered is: **${ign}**.`
         );
         client.channels
-          .get(config.dev.channelIds.hunters) // TODO: change in production
+          .get(process.env.HUNTER) // TODO: change in production
           .send(`${message.author} - ${id} - ${ign}`);
         member.addRole(role).catch(console.error);
       }
     }
 
     else if (team === 'survivor') {
-      let role = message.guild.roles.get(config.dev.roleId); // TODO: change in production
+      let role = message.guild.roles.get(process.env.QED); // TODO: change in production
       let member = message.member;
       if (message.member.roles.has(role.id)) {
         message.reply(
@@ -81,7 +98,7 @@ client.on('message', message => {
           `I've added you to the queue for **${team}**. Remember this is *NOT* a guarantee. Your ID you entered is: **${id}** and the IGN you entered is: **${ign}**.`
         );
         client.channels
-          .get(config.dev.channelIds.survivors) // TODO: change in production
+          .get(process.env.SURVIVOR) // TODO: change in production
           .send(`${message.author} - ${id} - ${ign}`);
         member.addRole(role).catch(console.error);
       }
@@ -90,10 +107,10 @@ client.on('message', message => {
   if (command === 'tourney') {
     let ign = args[0];
     let id  = args[1];
-    new Player({
-      ign: ign,
-      id: id
-    })
+    // new Player({
+    //   ign: ign,
+    //   id: id
+    // })
     message.reply(`Tourney test:
     IGN: ${ign}
     ID: ${id}
@@ -101,4 +118,4 @@ client.on('message', message => {
   }
 });
 
-client.login(config.dev.token); // TODO: change in production
+client.login(process.env.TOKEN); // TODO: change in production
