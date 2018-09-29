c9a40c323ba95cd2e2f15aac38130d4d8109f957
Patrick Black Jr <info@patrickblackjr.com>
2018-09-28 22:13:44 -0500

    big change check commit you lazy bum

--- index.js
+++ index.js
@@ -29,12 +29,6 @@ client.on('message', message => {
   
   const shortLinks = ["goo.gl", "shorte.st", "adf.ly", "bc.vc", "bit.ly", "bit.do", "soo.gd", "7.ly", "5.gp", "tiny.cc", "ouo.io", "zzb.bz", "adfoc.us", "my.su"]
   const swearWords = ["faggot", "gini", "kike", "n1gga", "n1gger", "nigg3r", "nigga", "nigger", "retard", "niqqa", "n1qqa", "niqqer", "n1qqer",]
-  const reaction = ["apple", "lol", "persway"]
-
-  if (message.content === 'apples') {
-    const apples = client.emojis.find(emoji => emoji.name === 'apple')
-    message.reply(`${apples}`)
-  }
 
   if (swearWords.some(word => message.content.toLowerCase().includes(word))) {
     message.delete();
@@ -66,47 +60,75 @@ client.on('message', message => {
     let team = args[0];
     let id   = args[1];
     let ign  = args[2];
-    let role = message.guild.roles.get(process.env.QED); // TODO: change in production
-    let member = message.member;
+    if (ign == undefined || id == undefined || team == undefined) {
+      message.reply(
+        `Uh oh! Looks like you forgot a parameter. Check your command and try again. Remember you need to specify a *TEAM*, *ID*, and *IGN*.\nThat looks like this: \`!custom CHOOSEATEAM YOURID YOURIGN\``
+      )
+      client.channels
+      .find(c => c.name === 'logs')
+      .send(`${message.author} tried so hard and got so far. But in the end, they can't type. Help them **join custom as a _${team}_**.`)
+      .catch(console.error)
+      return;
+    }
+    if (team == 'hunter' || team == 'survivor') {
+      let role = message.guild.roles.get(process.env.QED);
+      let member = message.member;
 
-    if (team === 'hunter') {
-      if (message.member.roles.has(role.id)) {
-        message.reply(
-          `You're already in the queue. You can't join again. Sorry!`
-        );
+      if (team === 'hunter') {
+        if (message.member.roles.has(role.id)) {
+          message.reply(
+            `You're already in the queue. You can't join again. Sorry!`
+          );
+        }
+        else {
+          message.reply(
+            `I've added you to the queue for **${team}**. Remember this is *NOT* a guarantee. Your ID you entered is: **${id}** and the IGN you entered is: **${ign}**.`
+          );
+          client.channels
+            .get(process.env.HUNTER)
+            .send(`${message.author} - ${id} - ${ign}`);
+          member.addRole(role).catch(console.error);
+        }
       }
-      else {
-        message.reply(
-          `I've added you to the queue for **${team}**. Remember this is *NOT* a guarantee. Your ID you entered is: **${id}** and the IGN you entered is: **${ign}**.`
-        );
-        client.channels
-          .get(process.env.HUNTER) // TODO: change in production
-          .send(`${message.author} - ${id} - ${ign}`);
-        member.addRole(role).catch(console.error);
+
+      else if (team === 'survivor') {
+        let role = message.guild.roles.get(process.env.QED);
+        let member = message.member;
+        if (message.member.roles.has(role.id)) {
+          message.reply(
+            `You're already in the queue. You can't join again. Sorry!`
+          );
+        } else {
+          message.reply(
+            `I've added you to the queue for **${team}**. Remember this is *NOT* a guarantee. Your ID you entered is: **${id}** and the IGN you entered is: **${ign}**.`
+          );
+          client.channels
+            .get(process.env.SURVIVOR)
+            .send(`${message.author} - ${id} - ${ign}`);
+          member.addRole(role).catch(console.error);
+        }
       }
     }
-
-    else if (team === 'survivor') {
-      let role = message.guild.roles.get(process.env.QED); // TODO: change in production
-      let member = message.member;
-      if (message.member.roles.has(role.id)) {
-        message.reply(
-          `You're already in the queue. You can't join again. Sorry!`
-        );
-      } else {
-        message.reply(
-          `I've added you to the queue for **${team}**. Remember this is *NOT* a guarantee. Your ID you entered is: **${id}** and the IGN you entered is: **${ign}**.`
-        );
-        client.channels
-          .get(process.env.SURVIVOR) // TODO: change in production
-          .send(`${message.author} - ${id} - ${ign}`);
-        member.addRole(role).catch(console.error);
-      }
+    else {
+      message.reply(
+        `Your team selection should either be **hunter** OR **survivor**. You entered \`${team}\``
+      )
+      return;
+    }
+  }
+  if (command === 'help') {
+    let whichCommand = args[0];
+    let whichSubCommand = args[1];
+    if (whichCommand === 'custom' && whichSubCommand === 'hunter') {
+        message.reply(`Copy this and fill in your information: \`!custom hunter 1234566 yourign\``);
+    }
+    if (whichCommand === 'custom' && whichSubCommand === 'survivor') {
+      message.reply(`Copy this and fill in your information: \`!custom survivor 1234566 yourign\``);
     }
   }
   if (command === 'tourney') {
     let ign = args[0];
-    let id  = args[1];
+    let id = args[1];
     // new Player({
     //   ign: ign,
     //   id: id
@@ -118,4 +140,4 @@ client.on('message', message => {
   }
 });
 
-client.login(process.env.TOKEN); // TODO: change in production
+client.login(process.env.TOKEN);
