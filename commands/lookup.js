2db88f9cadfcbe88d9f65f7ebc2c37bc25f60e57
Patrick Black Jr <info@patrickblackjr.com>
2018-10-04 09:37:14 -0500

    Update lookup

--- commands/lookup.js
+++ commands/lookup.js
@@ -12,12 +12,32 @@ module.exports = {
   args: true,
   cooldown: 5,
   execute(message, args, client) {
-    let lookupParam = args[0];
-    let lookupItem = args[1];
+    // let lookupParam = args[0];
+    // let lookupItem = args[1];
+    // let collection = db.collection('playerBase');
+
+    // return collection.find().toArray();
+
+    // playerBase.find({lookupParam: lookupParam}).toArray((err, result) => {
+    //   if (err) {
+    //     message.reply(`You aren't registered in the tourney!`);
+    //   }
+    //   if (result.length) {
+    //     message.reply(`Found: ${result.length}`);
+    //   }
+    //   else {
+    //     console.error(`no reuslt found`);
+    //   }
+    // })
+
     // const query = db.collection('playerBase').find({
     //   lookupParam: lookupItem
     // });
     // db.Player.find
+    if (!lookupItem) {
+      message.reply(`You didn't provide your IGN or ID.`)
+      return
+    }
     message.reply(`This command is still a work in progress and is not finished yet! DM <@136943278854504448> to have him check for you.\nCopy and paste this:\n\n\`Am I in the tournament entry list? ${lookupParam}: ${lookupItem}\``)
   },
 };
