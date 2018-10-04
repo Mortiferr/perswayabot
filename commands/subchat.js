c48c4645b746368d94a727fc89b5782831ac20bf
Patrick Black Jr <info@patrickblackjr.com>
2018-10-04 09:37:25 -0500

    Add subchat command verification

--- commands/subchat.js
+++ commands/subchat.js
@@ -2,15 +2,29 @@
 module.exports = {
   name: 'subchat',
   description: 'Staff only.',
-  aliases: ['commands'],
-  usage: '[command name]',
+  usage: '',
   cooldown: 2,
   execute(message, args, client) {
     if (message.member.hasPermission('KICK_MEMBERS')) {
-      client.channels
-        .find(c => c.name === 'announcements')
-        .send(`<@&489224374885416970> - Persway is doing a subscriber chat! Join to ask questions.`)
-        .catch(console.error)
+      message.channel.send('Are you sure you want to send an announcement to all subscribers? Type `YES` in ALL CAPS to confirm.')
+        .then(() => {
+          message.channel.awaitMessages(response => response.content === 'YES', {
+            max: 1,
+            time: 30000,
+            errors: ['time'],
+          })
+          .then((collected) => {
+            message.channel.send(`Sending!`);
+            client.channels
+              .find(c => c.name === 'announcements')
+              .send(`<@&489224374885416970> - Persway is doing a subscriber chat! Join to ask questions.`)
+              .catch(console.error)
+            message.channel.send(`Sent!`);
+          })
+          .catch(() => {
+            message.channel.send(`You didn't respond within the time limit. Announcement not sent.`)
+          })
+        })
     }
   },
 }; 
