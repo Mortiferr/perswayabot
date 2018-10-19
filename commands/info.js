af3b2da4c462e24e7bd147b630a76110e8a5d10d
Patrick Black Jr <info@patrickblackjr.com>
2018-10-18 22:42:40 -0500

    remove extranous comment

--- commands/info.js
+++ commands/info.js
@@ -20,7 +20,6 @@ exports.run = (client, message, args, level) => {
   }
 
   if (whichCommand === 'tourney') {
-    // message.channel.send(`Bacon ipsum dolor amet turducken filet mignon strip steak jerky, ham hock tri-tip swine andouille ground round meatloaf beef ribs pork loin. Shoulder buffalo cupim porchetta bresaola, beef ribs venison prosciutto chuck brisket chicken flank. Doner ribeye venison kevin prosciutto chicken fatback landjaeger. Turducken spare ribs shoulder, andouille pancetta meatloaf pastrami ground round turkey pork belly tri-tip tail short ribs. Salami andouille capicola hamburger jowl, ham hock t-bone short ribs cow biltong. Salami bresaola andouille beef meatball tongue ham jerky doner pork loin.`);
     const infoEmbed = {
       color: 0xFCE300,
       author: {
