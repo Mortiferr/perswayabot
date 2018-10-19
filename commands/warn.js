f37cfa97403d2c7047e84772004cf86d7d3799f7
Patrick Black Jr <info@patrickblackjr.com>
2018-10-19 15:24:22 -0500

    wip warn

--- commands/warn.js
+++ commands/warn.js
@@ -1,10 +1,40 @@
 const mongoose = require('mongoose');
+const ObjectID = require('mongodb').ObjectID;
+let db = mongoose.connection;
+
 exports.run = (client, message, args, level) => {
+
   const Warning = mongoose.model('Warning', {
     user: String,
     mod: String,
-    reason: String
-  })
+    reason: String,
+  });
+
+  let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let reason = message.content.split(' ').slice(2).join(' ');
+  let mod = message.author;
+
+  const warnings = new Warning({
+    user: user,
+    mod: mod,
+    reason: reason,
+    __id: new ObjectID()
+  });
+
+
+  db.collection('warnings').insertOne(warnings, (err, res) => {
+    client.logger.log(`Inserted the warning in the db.`, 'debug');
+  });
+
+  // let cursor = db.collection('warnings').find();
+  // cursor.forEach((doc, err) => {
+  //   resultArray.push(doc);
+  // });
+
+  warnings
+    .save()
+    .then(() => client.logger.log(`${mod} warned ${user} for "${reason}".`, "debug"));
+
 }
 
 exports.conf = {
